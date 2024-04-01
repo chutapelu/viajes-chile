218ad7a5e1af2e6d8e1cab39e572124efcd6a9e8
chutapelu <juanlinaresc89@gmail.com>
2024-04-01 17:56:42 -0300

    agregados los assets

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function() {
+    $(".navbar-nav a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+    $(".card img").hover(function(){
+        $(this).next(".card-body").toggle();
+    });
+});
